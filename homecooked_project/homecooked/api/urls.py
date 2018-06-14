ad344e3722bb1c8f6b43b79327103420ee97a003
mainib <biniam.hagos17@gmail.com>
2018-06-13 21:43:32 -0700

    Django_Rest is working. able to make api call for all models. need more work with auth. so that a user does not edit another users models.

---
+++ homecooked_project/homecooked/api/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from .views import UserViewSet, GroupViewSet, ProfileViewSet, KitchenViewSet, DishViewSet,OrderViewSet
+
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'profiles', ProfileViewSet)
+router.register(r'kitchens', KitchenViewSet)
+router.register(r'dishes', DishViewSet)
+router.register(r'orders', OrderViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+ url(r'^', include(router.urls))
+]
