a3e08604578a992060c668f63207a520ee6bf609
DaltonHart <daltonvaren@gmail.com>
2018-06-15 13:36:00 -0700

    fileupload working with template cooks

--- homecooked_project/homecooked/views.py
+++ homecooked_project/homecooked/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import registrationForm, ProfileForm, KitchenForm, DishForm
-from .models import User, Kitchen
+from .models import User, Kitchen, Profile, Dish
 # Create your views here.
 def landing(request):
     return render(request, 'homecooked/landing.html')
@@ -36,7 +36,10 @@ def profile_create(request):
     return render(request, 'homecooked/profileform.html', {'form': form})
 
 def kitchen(request):
-    return render(request, 'homecooked/userIndex.html')
+    kitchens = Kitchen.objects.all()
+    dishes = Dish.objects.all()
+    print('look here',dishes)
+    return render(request, 'homecooked/userIndex.html', {'kitchens': kitchens, 'dishes': dishes})
 
 def kitchen_create(request):
     if request.method == 'POST':
@@ -57,7 +60,6 @@ def dish_create(request):
             post = form.save(commit=False)
             owner = request.user
             foundkitchen = Kitchen.objects.filter(owner = owner)
-            print('HERE IT IS LOOK HERE', foundkitchen.first())
             post.kitchen = foundkitchen.first()
             post.save()
             return redirect('kitchen')
