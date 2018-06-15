2d75580c3a2e0339df215a56ec8e6ca925cb38f1
mainib <biniam.hagos17@gmail.com>
2018-06-15 10:25:03 -0700

    commit b4 pull

--- homecooked_project/homecooked/static/js/landing.js
+++ homecooked_project/homecooked/static/js/landing.js
@@ -1,11 +1,24 @@
 $(document).ready(function($){
-	console.log('sanity check')
-	details = $('.overview').children().find('.detail')
-	details.hide()
-	$('.showbtn').on('click', (e)=>{
-		$(details).slideToggle( "slow" );
+	console.log('sanity check one')
+	cook = $('.cook')
+	customer = $('.customer')
+	cookdetails = $('.cook').find('.detail')
+	customerdetails = $('.customer').find('.detail')
+	// cookdetails.hide()
+	// customerdetails.hide()
+	
+	$('#custBtnDiv').on('click', (e)=>{
+		console.log('check')
+		// cook.fadeToggle('slow', 'linear')
 	})
-
+	$('#cookBtnDiv').on('click', (e)=>{
+		console.log('check')
+		// var toggleWidth = $("#toggle").width() == 300 ? "200px" : "300px";
+		var togglewidth = $(cook).width() == "100%" ? "50%" : "70%";
+		// $('.cookDetail').toggle().show();
+		// cook.toggle().animate({ width: toggleWidth });
+		// customer.fadeToggle('slow', 'linear')
+	})
 
 
 });
