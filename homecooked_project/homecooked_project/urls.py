23f7756ec7af8178731635d02150d08f5f8a396e
int-JP <jeanpauldualan@gmail.com>
2018-06-18 12:04:36 -0700

    try

--- homecooked_project/homecooked_project/urls.py
+++ homecooked_project/homecooked_project/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url, include, static
+from django.contrib.auth import views as auth_views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
